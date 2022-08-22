2eb150245e4affb2870f7e5ca5d5ef0fde718fc7
robertsteele7 <atpcycle7@gmail.com>
2022-08-22 11:30:57 -0500

    first commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            var quotient = a / b;            
+            var remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine("Enter radius of the Circle");           
+            var radius = double.Parse(Console.ReadLine());
+            var AreaOfCircle = Math.PI * radius * radius;
+            Console.WriteLine("Area of the Circle with radius {0} is {1}", radius, AreaOfCircle);
+            
+
+        }
+    }
+}
